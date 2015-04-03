d3d3727e59d9f1fb11af4508d3aafdd674c2665e
davehand <davehand13@gmail.com>
2015-04-03 00:28:38 -0400

    Added feature testing for users

--- monopoly/app/models/user.rb
+++ monopoly/app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   has_many :games, :through => :players
 
   has_secure_password
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
