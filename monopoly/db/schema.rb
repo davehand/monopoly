4ab157b3db52877b44604b5995b3c7d7054c3766
Timmy Stephani [Mac Laptop] <timmy.stephani@gmail.com>
2015-04-02 22:06:16 -0400

    fixed schema

--- monopoly/db/schema.rb
+++ monopoly/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150403002709) do
-=======
 ActiveRecord::Schema.define(version: 20150403005852) do
->>>>>>> add created user id to games. ui updates to game show
 
   create_table "games", force: :cascade do |t|
     t.integer  "current_player_id"
@@ -23,10 +19,7 @@ ActiveRecord::Schema.define(version: 20150403005852) do
     t.integer  "free_parking_pot"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-<<<<<<< HEAD
-=======
     t.integer  "created_user_id"
->>>>>>> add created user id to games. ui updates to game show
   end
 
   create_table "players", force: :cascade do |t|
@@ -43,14 +36,6 @@ ActiveRecord::Schema.define(version: 20150403005852) do
   end
 
   create_table "users", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-
-=======
     t.string   "email",      null: false
     t.string   "password",   null: false
     t.datetime "created_at", null: false
@@ -59,5 +44,4 @@ ActiveRecord::Schema.define(version: 20150403005852) do
 
   add_index "users", ["email"], name: "index_users_on_email"
 
->>>>>>> add created user id to games. ui updates to game show
 end
