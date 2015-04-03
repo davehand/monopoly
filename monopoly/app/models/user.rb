7844d590145d0372031a848640c5c373725ea529
davehand <davehand13@gmail.com>
2015-04-02 21:17:32 -0400

    Created the users and sessions

--- monopoly/app/models/user.rb
+++ monopoly/app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  validates :email, presence:true
-  validates :password, presence:true
+  has_secure_password
+  validates :email, presence: true
 end
--- monopoly/db/schema.rb
+++ monopoly/db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402231935) do
+ActiveRecord::Schema.define(version: 20150403002709) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      null: false
-    t.string   "password",   null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
-
-  add_index "users", ["email"], name: "index_users_on_email"
 
 end
