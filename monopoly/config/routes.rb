bb8de0d97b85f82e375b83c5ca7203c605f1a8dd
davehand <davehand13@gmail.com>
2015-04-02 21:40:05 -0400

    Fixed user create issue

--- monopoly/config/routes.rb
+++ monopoly/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
 
   resources :users
 
