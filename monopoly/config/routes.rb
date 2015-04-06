986ceb8eb0d6aa771bfc908a2ddaa7e1d4eef591
Timmy Stephani [Mac Laptop] <timmy.stephani@gmail.com>
2015-04-06 00:47:56 -0400

    add ui logic, api, and board space

--- monopoly/config/routes.rb
+++ monopoly/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    namespace :v1 do
+      resources :games
+      resources :board_spaces
+    end
+  end
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
