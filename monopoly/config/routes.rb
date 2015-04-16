76133ce60e76ea9946bda2142a33009a36670159
davehand <davehand13@gmail.com>
2015-04-16 19:24:55 -0400

    Added owned properties model and enabled button

--- monopoly/config/routes.rb
+++ monopoly/config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :games do 
-        member do 
+      resources :games do
+        member do
           get 'roll_dice'
         end
       end
       resources :board_spaces
+      resources :players
     end
   end
 
